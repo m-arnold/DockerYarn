734a4746ee8f78539cf1225612df4d44e7930570
Michael Arnold <marnold@cj.com>
2017-04-10 09:59:00 -0700

    change name of image

--- build.sh
+++ build.sh
@@ -2,18 +2,20 @@
 
 touch yarn.lock
 
+DOCKERNAME="cjengineering/yarn-builder"
+
 # Init empty cache file
 if [ ! -f .yarn-cache.tgz ]; then
   echo "Init empty .yarn-cache.tgz"
   tar cvzf .yarn-cache.tgz --files-from /dev/null
 fi
 
-docker build . -t yarn-demo
+docker build . -t $DOCKERNAME
 
-docker run --rm --entrypoint cat yarn-demo:latest /tmp/yarn.lock > /tmp/yarn.lock
+docker run --rm --entrypoint cat $DOCKERNAME:latest /tmp/yarn.lock > /tmp/yarn.lock
 if ! diff -q yarn.lock /tmp/yarn.lock > /dev/null  2>&1; then
   echo "Saving Yarn cache"
-  docker run --rm --entrypoint tar yarn-demo:latest czf - /root/.yarn-cache/ > .yarn-cache.tgz
+  docker run --rm --entrypoint tar $DOCKERNAME:latest czf - /root/.yarn-cache/ > .yarn-cache.tgz
   echo "Saving yarn.lock"
   cp /tmp/yarn.lock yarn.lock
 fi
